26188b8593f01d336b3f42a8b96c0855b1ca858a
NomanTrips <brianscott0017@yahoo.com>
2020-12-15 14:12:40 -0500

    app run change

--- python_code/sample_request.py
+++ python_code/sample_request.py
@@ -1,5 +1,7 @@
 import requests
 
-resp = requests.post("http://localhost:5000/predict",
-                     files={"file": open('../src/assets/hawk-three.jpg','rb')})
+#resp = requests.post("http://localhost:5000/predict",
+#                     files={"file": open('../src/assets/hawk-three.jpg','rb')})
+resp = requests.post("http://52.37.86.143:5000/predict",
+                     files={"file": open('../src/assets/avatars/eagle-avatar.png','rb')})
 print(resp.json())
